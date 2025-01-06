585b3125bbb292315b317154e9bc1f3ff6fe04fa
Keven <kevenchen777@gmail.com>
2025-01-06 11:49:03 -0400

    General Sidebar Hecho

--- aparta+/src/components/generalSidebar.jsx
+++ aparta+/src/components/generalSidebar.jsx
@@ -1,13 +1,36 @@
 import "+/generalSidebar.component.scss";
-function GeneralSidebar() {
+
+const Sidebar = () => {
   return (
-    <div className="generalSidebar">
-      <button onClick={() => window.history.back()} className="back-btn">
-        ←
-      </button>
+    <div className="sidebar">
+      <nav className="sidebar__nav">
+        <div className="sidebar__item">
+          <a href="/dashboard" className="sidebar__link">
+            Dashboard
+          </a>
+        </div>
+        
+        <div className="sidebar__item">
+          <a href="/propiedades" className="sidebar__link sidebar__link--active">
+            Propiedades
+          </a>
+        </div>
+
+        <div className="sidebar__item">
+          <a href="/inquilinos" className="sidebar__link">
+            Inquilinos
+          </a>
+        </div>
+
+        <div className="sidebar__item">
+          <a href="/reportes" className="sidebar__link">
+            Reportes
+          </a>
+        </div>
+      </nav>
     </div>
   );
-}
+};
 
-export default GeneralSidebar;
+export default Sidebar;
 
