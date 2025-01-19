12a69afe0afd23b4f7f33d76c4a7069a1b220be4
Aquile-User <Aquilenyisuero@gmail.com>
2025-01-19 15:12:31 -0400

    feat: Rediseñar la vista de Apartamentos con nuevas secciones y botones

--- aparta+/src/app/routes/verApartamentos.jsx
+++ aparta+/src/app/routes/verApartamentos.jsx
@@ -1,17 +1,44 @@
 import MainView from "*/mainView";
+import ApartamentCard from "*/apartmentCard"; 
+import Button from "*/button";
 import React from "react";
+import "+/verApartamentos.scss";
 
 const VerApartamentos = () => {
     return (
         <MainView sidebarType="thin">
-            <div className="View-title">
-                <h1>B4</h1>
-            </div>
-            <div className="View-imagenes">
-                <img className="imagenes" src="./src/assets/house.png" alt="house"  imagenes />
-                <img className="imagenes" src="./src/assets/house1.png" alt="house"  imagenes />
-                <img className="imagenes" src="./src/assets/house3.png" alt="house"  imagenes />
-                <img className="imagenes" src="./src/assets/house4.jpg" alt="house"  imagenes />
+            <div className="page-container">
+
+                <h1 className="view-title">B4</h1>
+
+                <section className="first-content">
+                    <div className="view-imagenes">
+                        <img className="imagenes" src="./src/assets/house.png" alt="house" />
+                        <img className="imagenes" src="./src/assets/house1.png" alt="house" />
+                        <img className="imagenes" src="./src/assets/house3.png" alt="house" />
+                        <img className="imagenes" src="./src/assets/house4.jpg" alt="house" />
+                        <div className="buttons">
+                            <Button text="Eliminar" color="blue" width="150px"></Button>
+                            <Button text="Editar" color="green" width="150px"></Button>
+                        </div>
+                    </div>
+                    <div className="view-apartamentos">
+                        <ApartamentCard></ApartamentCard>
+                        <ApartamentCard></ApartamentCard>
+                    </div>
+                </section>
+
+                <section className="second-content">
+                    <header className="info-title"><h2>Información</h2></header>
+                    <body>
+                        <p><strong>Inquilino:</strong> Aquilenyi Suero De Los Santos</p>
+                        <p><strong>Monto:</strong> 30,000</p>
+                        <div>
+                            
+                        </div>
+
+                    </body>
+                </section>
             </div>
         </MainView>
     );
