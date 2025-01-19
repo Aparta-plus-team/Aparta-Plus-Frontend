7e50507cabe8d61affe08ad300fddf8ca216f44d
Aquile-User <Aquilenyisuero@gmail.com>
2025-01-19 01:52:10 -0400

    Añade el componente Matrix con mejoras en la selección de años y estados mensuales; incluye ajustes en estilos

--- aparta+/src/components/Matrix.jsx
+++ aparta+/src/components/Matrix.jsx
@@ -6,8 +6,8 @@ return (
     <header className="matrix-header">
         <h1 className='matrix-title'>Estadisticas</h1>
         <div className="matrix-select">    
-          <label for="Años">Año</label> 
-          <select id="Años" name="Años"> 
+          <select className='select-box' name="Años"> 
+            <option value="">Año</option> 
             <option value="Año1">2025</option> 
             <option value="Año2">2024</option> 
             <option value="Año3">2023</option> 
@@ -17,18 +17,18 @@ return (
     </header>
     <article className="matrix-content">
       <ul className="matrix-months">
-        <li className='month'>Enero<br/></li>
-        <li className='month'>Febrero<br/></li>
-        <li className='month'>Marzo<br/></li>
-        <li className='month'>Abril<br/><p className='matrix-state'>Pagado</p></li>
-        <li className='month'>Mayo<br/></li>
-        <li className='month'>Junio<br/></li>
-        <li className='month'>Julio<br/></li>
-        <li className='month'>Agosto<br/></li>
-        <li className='month'>Septiembre<br/></li>
-        <li className='month'>Octubre<br/></li>
-        <li className='month'>Noviembre<br/></li>
-        <li className='month'>Diciembre<br/></li>
+        <li className='month'>Enero<br/><div className='matrix-state-pagado'><p>Pagado</p></div></li>
+        <li className='month'>Febrero<br/><div className='matrix-state-desocupado'><p>Desocupado</p></div></li>
+        <li className='month'>Marzo<br/><div className='matrix-state-adelantado'><p>Adelantado</p></div></li>
+        <li className='month'>Abril<br/><div className='matrix-state-pagado'><p>Pagado</p></div></li>
+        <li className='month'>Mayo<br/><div className='matrix-state-desocupado'><p>Desocupado</p></div></li>
+        <li className='month'>Junio<br/><div className='matrix-state-adelantado'><p>Adelantado</p></div></li>
+        <li className='month'>Julio<br/><div className='matrix-state-pagado'><p>Pagado</p></div></li>
+        <li className='month'>Agosto<br/><div className='matrix-state-desocupado'><p>Desocupado</p></div></li>
+        <li className='month'>Septiembre<br/><div className='matrix-state-adelantado'><p>Adelantado</p></div></li>
+        <li className='month'>Octubre<br/><div className='matrix-state-pagado'><p>Pagado</p></div></li>
+        <li className='month'>Noviembre<br/><div className='matrix-state-desocupado'><p>Desocupado</p></div></li>
+        <li className='month'>Diciembre<br/><div className='matrix-state-adelantado'><p>Adelantado</p></div></li>
       </ul>
     </article>
   </section>
