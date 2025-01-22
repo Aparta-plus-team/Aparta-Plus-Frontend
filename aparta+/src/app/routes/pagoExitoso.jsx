63c01ea32ca6996ed472daa8260e374557911bc6
Keven <kevenchen777@gmail.com>
2025-01-21 23:18:48 -0400

    feat: Ruteo de Volver al Inicio

--- aparta+/src/app/routes/pagoExitoso.jsx
+++ aparta+/src/app/routes/pagoExitoso.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import "+/pagoExitoso.scss";
 
 const PagoExitoso = () => {
-    return (
-        <div class='body'>
-            <div class="confirmation-card">
-                <div class="success-icon"></div>
-                <h1 class="title">¡Pago Exitoso!</h1>
-                <p class="message">Tu pago ha sido procesado correctamente.</p>
-                
-                <div class="details">
-                    <p>Monto pagado:</p>
-                    <div class="amount">$149.99</div>
-                    <p class="transaction-id">ID de Transacción: #123456789</p>
-                </div>
+  return (
+    <div class="body">
+      <div class="confirmation-card">
+        <div class="success-icon"></div>
+        <h1 class="title">¡Pago Exitoso!</h1>
+        <p class="message">Tu pago ha sido procesado correctamente.</p>
 
-                <button class="button">Volver al Inicio</button>
-            </div>
+        <div class="details">
+          <p>Monto pagado:</p>
+          <div class="amount">$149.99</div>
+          <p class="transaction-id">ID de Transacción: #123456789</p>
         </div>
-    );
-}
+        <a href="/dashboard">
+          <button class="button">Volver al Inicio</button>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default PagoExitoso;
